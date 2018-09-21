16773cca5027c6f73e7d5fd4145533964ee80078
gkeswani92 <gauravkeswani92@gmail.com>
2018-09-20 19:18:12 -0700

    Using react-charts to plot the distribution of responses for a question on the Board

--- src/Board.js
+++ src/Board.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
 import Display from './parts/Display';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 
 class Board extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
+        this.formatBarGraphData = this.formatBarGraphData.bind(this);
     }
+
+    formatBarGraphData(results) {
+        console.log(results);
+        return Object.keys(results).map((choice) => {
+            return {
+                label: choice,
+                value: results[choice],
+            };
+        });
+    }
+
     render() { 
         return (
             <div id="scoreboard">
                 <Display if={this.props.status === 'connected' && this.props.currentQuestion}>
-                    <h3>{this.props.currentQuestion.q}</h3>
-                    <p>{JSON.stringify(this.props.results)}</p>
+                    <BarChart data={this.formatBarGraphData(this.props.results)}
+                              title={this.props.currentQuestion.q}
+                              height={window.innerHeight * 0.6}
+                              width={window.innerWidth * 0.9}
+                    >
+                        <XAxis dataKey="label"/>
+                        <YAxis dataKey="value" />
+                        <Tooltip/>
+                        <Legend />
+                        <Bar dataKey="value" fill="#8884d8" />
+                    </BarChart>
                 </Display>
 
                 <Display if={this.props.status === 'connected' && !this.props.currentQuestion}>
